da069bc7c38c9551e47ec8f26686843349f0fd7c
J-jianfei <jiangjianfeia@gmail.com>
2024-05-07 00:05:32 +0200

    add find max

--- findMaxTractionMagnitudeROI.m
+++ findMaxTractionMagnitudeROI.m
@@ -1,32 +1,36 @@
-function varargout = findMaxTractionMagnitude(varargin)
+function varargout = findMaxTractionMagnitudeROI(varargin)
     [filename,path] = uigetfile('*.mat','Select the TFM output file from TFM_main script');
     load(fullfile(path,filename),"TF_attempt");
+
+    manual_selection = 0;
+
     
     [imcellname,imcellpath] = uigetfile('*.tif','Select the Cell image(.tif) to select ROI for maximum traction finding');
-    if ~exist(fullfile(imcellpath,imcellname)) || imcellname == 0
+    if ~exist(fullfile(imcellpath,imcellname), 'file') || isequal(imcellname, 0)
         disp('No image is selected, will try to use the entire region');
     else
+        manual_selection = 1;
         imcell = imread(fullfile(imcellpath,imcellname));
         figure, imshow(imadjust(mat2gray(imcell))); colormap gray;
         % Enable rectangle tool for ROI selection
-        rect = drawrectangle;
-        % Wait for user to double click to confirm selection
-        wait(rect, 'double');
+        % Enable polygon tool for ROI selection
+        poly = drawpolygon;
 
-        % Get the final position of the rectangle
-        roi = rect.Position;
+        click1 = waitforbuttonpress;
+        click2 = waitforbuttonpress;
+        if(click1 == 0 && click2 == 0)
+            disp('Polygon selection is done');
+        end
+       
 
-        % Convert ROI coordinates to pixel indices
-        x1 = round(roi(1));
-        y1 = round(roi(2));
-        x2 = round(roi(1) + roi(3));
-        y2 = round(roi(2) + roi(4));
-        % Perform further processing on the cropped image
-        % ...
+        % Get the final position of the polygon
+        roi = poly.Position;
+
+        close(gcf);
     end
 
 
-    filenameWithoutSuffix = strrep(filename, '_TFM_output*.mat', '');
+    filenameWithoutSuffix = extractBefore(filename, '_TFM_output');
     prompt = {'Specify the method from which the result are obtained and you want to analyze (B for Bayesian, R for Regularized)', ...
     'Specify the attempt index if you have multiple attempts on that method', ...
     'Specify the frame number for which you want to find the maximum traction magnitude (0 for finding the maximum over all frames)'};
@@ -38,20 +42,24 @@ function varargout = findMaxTractionMagnitude(varargin)
     j = str2double(userInput{2});
     iframe = str2double(userInput{3});
 
+
     if strcmpi(method,'Bayesian') || strcmpi(method,'Bay') || strcmpi(method,'B')
         if (~isempty(TF_attempt.Bayesian))
-                for frame = 1:length(TF_attempt.Bayesian(j).TFM_results)
-                    if ~exist("x1","var") 
-                        x1 = min(TF_attempt.Bayesian(j).TFM_results(frame).pos(:,1));
-                        y1 = min(TF_attempt.Bayesian(j).TFM_results(frame).pos(:,2));
-                        x2 = max(TF_attempt.Bayesian(j).TFM_results(frame).pos(:,1));
-                        y2 = max(TF_attempt.Bayesian(j).TFM_results(frame).pos(:,2));
+            TFM_results = TF_attempt.Bayesian(j).TFM_results;
+                for frame = 1:length(TFM_results)
+                    if ~exist("roi","var") 
+                        x1 = min(TFM_results(frame).pos(:,1));
+                        y1 = min(TFM_results(frame).pos(:,2));
+                        x2 = max(TFM_results(frame).pos(:,1));
+                        y2 = max(TFM_results(frame).pos(:,2));
+                        roi = [x1 y1; x2 y1; x2 y2; x1 y2];
                     end
-                    traction_magnitude = TF_attempt.Bayesian(j).TFM_results(frame).traction_magnitude();
+                    in_id = find(inpolygon(TFM_results(frame).pos(:,1),TFM_results(frame).pos(:,2),roi(:,1),roi(:,2)));
+                    traction_magnitude = TFM_results(frame).traction_magnitude(in_id);
                     [maxTmagAtFrametmp,idxtmp]= max(traction_magnitude(:));
                     maxTmagAtFrame(frame) = maxTmagAtFrametmp(1);
                     idx = idxtmp(1);
-                    posmaxAtFrame(frame,:) = TF_attempt.Bayesian(j).TFM_results(frame).pos(idx,:);
+                    posmaxAtFrame(frame,:) = TFM_results(frame).pos(in_id(idx),:);
                 end
                 
                 if iframe == 0
@@ -70,12 +78,14 @@ function varargout = findMaxTractionMagnitude(varargin)
         end
     elseif strcmpi(method,'Regularized') || strcmpi(method,'Reg') || strcmpi(method,'R')
         if (~isempty(TF_attempt.Regularized))
-            for frame = 1:length(TF_attempt.Regularized(j).TFM_results)
-                traction_magnitude = TF_attempt.Regularized(j).TFM_results(frame).traction_magnitude;
-                [maxTmagAtFrametmp(frame),idxtmp] = max(traction_magnitude(:));
+            TFM_results = TF_attempt.Regularized(j).TFM_results;
+            for frame = 1:length(TFM_results)
+                traction_magnitude = TFM_results(frame).traction_magnitude;
+                in_id = find(inpolygon(TFM_results(frame).pos(:,1),TFM_results(frame).pos(:,2),roi(:,1),roi(:,2)));
+                [maxTmagAtFrametmp(frame),idxtmp] = max(traction_magnitude(in_id));
                 maxTmagAtFrame(frame) = maxTmagAtFrametmp(1);
                 idx = idxtmp(1);
-                posmaxAtFrame(frame,:) = TF_attempt.Regularized(j).TFM_results(frame).pos(idx,:);
+                posmaxAtFrame(frame,:) = TF_attempt.Regularized(j).TFM_results(frame).pos(in_id(idx),:);
             end
             if iframe == 0
                 [maxTmagtmp,idxtmp] = max(maxTmagAtFrame);
@@ -108,6 +118,17 @@ function varargout = findMaxTractionMagnitude(varargin)
         fprintf(fileID,'%s, %f, %f, %f, %d, %d, %s \n',filenameWithoutSuffix,maxTmag,posmax(1),posmax(2),iframe,j,method);
     end
     
+    fclose(fileID);
 
+    if(manual_selection)
+        
+        roi_filename = strcat(filenameWithoutSuffix, "_ROI.txt");
+       % if ~exist(fullfile(savepath,roi_filename),'file')
+            roi_fileId = fopen(fullfile(savepath,roi_filename),'w');
+            fprintf(roi_fileId,'Vertices positions: x, y\n');
+            fprintf(roi_fileId,'%f, %f\n',roi(:,1),roi(:,2));
+            fclose(roi_fileId);
+        % end
+    end
 
 end 
